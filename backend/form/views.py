6419751b1d41ec4cb1ef9f691383f84f694dc3ee
lucien <zimwolfgang24@gmail.com>
2023-01-25 05:09:35 -0400

    feat: form created

--- backend/form/views.py
+++ backend/form/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import Form
 
 
 def home(request):
-    return HttpResponse("test")
+    formModel = Form()
+    return render(request, "home.html", {"form": formModel})
+
+
+def data_response(request):
+    formResponse = Form(request.POST)
+    codeData = formResponse.data["code"]
+    return HttpResponse(codeData)
